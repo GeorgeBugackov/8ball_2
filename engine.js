c382ff49547a3b7de4117b9c004871a17c94688f
George Bugackov <george.bugackov@gmail.com>
2014-12-12 17:13:41 +0200

    ball CSS animation created

--- engine.js
+++ engine.js
@@ -1,12 +1,32 @@
 window.addEventListener('load', turnOn);
 
 function turnOn () {
-	var answers = ['Yes', 'No'],
-		answer = Math.round(Math.random());
+	var ball = document.querySelector('.ball'),
+		wrapper = document.querySelector('.wrapper');
 
-	document.getElementsByClassName('wrapper')[0].addEventListener('click', showAnswer);
+	wrapper.addEventListener('click', showAnswer);
+	// ball.addEventListener('mouseover', shake);
 
 	function showAnswer () {
+		var answers = ['Yes', 'No'],
+		answer = Math.round(Math.random());
+		console.log(answers[answer]);
+	}
+
+	function shake () {
+
+		
+			if (ball.style.top == 150) {
+				for (i = 0; i < 5; i++) {
+					ball.style.top = ball.style.top - 10;
+					console.log("-10 :: " + ball.style.top);
+				}
+			} else if (ball.style.top == 100) {
+				for (i = 0; i < 5; i++) {
+					ball.style.top = ball.style.top + 10;
+					console.log("+10 :: " + ball.style.top);
+				}
+			}
 
 	}
 
