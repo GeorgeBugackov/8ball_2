520dc65e2d3d0d0cc8aa8d8ca4068d85b36b6b8a
George Bugackov <george.bugackov@gmail.com>
2014-12-13 15:10:58 +0200

    text animation added

--- engine.js
+++ engine.js
@@ -12,13 +12,27 @@ function turnOn () {
 		answer = Math.round(Math.random());
 
 		wrapper.removeEventListener('click', showAnswer);
-		
+
+		fading(true);
+
 		text.innerText = answers[answer];
 
 		setTimeout(function() {
+			fading(false);
 			text.innerText = 8;
 			wrapper.addEventListener('click', showAnswer);
 		}, 1000);
+	}
+
+	function fading (toggle) {
+		if (toggle) {
+			text.className += (' fader');
+			console.log("fader added");
+		} else {
+			text.className = '';
+			text.className = 'text';
+			console.log("fader removed");
+		}
 	}
 
 }
